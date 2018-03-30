3a560abac2f62e2e80e7af986834651a0377471f
Ben Martinez-Bateman <ben@alwaysblank.org>
2018-03-30 16:54:40 -0700

    Better interface for manually triggering button toggles.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import shortid from 'shortid'
 import './index.css'
 
 
-const elName = `site-navigation`
+const elName = `site-navigation`;
 
-function getElements(navigator)
-{
+function getElements(navigator) {
   return {
     branding: navigator.querySelector(`[data-branding]`) || navigator.querySelector(`.branding`),
     menu: navigator.querySelector(`[data-menu]`) || navigator.querySelector(`.menu`),
@@ -15,22 +14,20 @@ function getElements(navigator)
 }
 
 
-function toggleMenu(el)
-{
+function toggleMenu(el) {
   el.hidden = !el.hidden;
 
   // Create CustomEvent to fire later.
-  let menuState = new CustomEvent(`menu-state`, { "bubbles": true, "cancelable": true,})
+  let menuState = new CustomEvent(`menu-state`, {"bubbles": true, "cancelable": true,});
 
   // Fire CustomEvent to let everyone know we changed state.
   el.dispatchEvent(menuState)
 }
 
 
-function handleMenuToggle(e)
-{
-  let menu = e.target
-  if (this.getAttribute(`id`) === e.explicitOriginalTarget.getAttribute(`aria-controls`)) {
+function handleMenuToggle(e) {
+  let menu = e.target;
+  if (this.getAttribute(`id`) === e.detail.menuid) {
     toggleMenu(menu)
   }
 }
@@ -40,8 +37,7 @@ function handleMenuToggle(e)
  * `toggle-state` event on it.
  * @param {HTMLElement} el The button we want to toggle.
  */
-function toggleButton(el)
-{
+function toggleButton(el) {
   // Determine the current state of this button.
   let expanded = el.getAttribute('aria-expanded') === 'true' || false;
 
@@ -50,10 +46,10 @@ function toggleButton(el)
   el.setAttribute('aria-expanded', !expanded);
 
   // Change the text of the button to the other thing.
-  el.textContent = expanded ? el.dataset.closed : el.dataset.opened
+  el.textContent = expanded ? el.dataset.closed : el.dataset.opened;
 
   // Create CustomEvent to fire later.
-  let toggleState = new CustomEvent(`toggle-state`, { "bubbles": true, "cancelable": true,})
+  let toggleState = new CustomEvent(`toggle-state`, {"bubbles": true, "cancelable": true,});
 
   // Fire CustomEvent to let everyone know we changed state.
   el.dispatchEvent(toggleState)
@@ -61,77 +57,81 @@ function toggleButton(el)
 
 /**
  * Handles changes to the button when it's toggled.
- * @param {HTMLElement} el The button being toggled.
- * @param {Object} settings Any settings for this button.
+ * @param e
  */
-function handleButtonToggle(e)
-{
+function handleButtonToggle(e) {
   // Set el.
-  let el = e.target
+  let el = e.target;
+  let menuid = e.detail.menuid;
 
   // Toggle button state.
-  toggleButton(el)
+  toggleButton(el);
 
   // Fire the event on the menu we want to toggle and swap
   // the menu's state.
   // We're doing both in here because we want menu state to be
   // inextricably tied to button state.
-  let menuToggled = new CustomEvent(`menu-toggled`, { "bubbles": true, "cancelable": true, })
-  let menu = document.getElementById(el.getAttribute(`aria-controls`))
+  let menuToggled = new CustomEvent(`menu-toggled`, {
+    "bubbles": true,
+    "cancelable": true,
+    "detail": {"menuid": menuid}
+  });
+  let menu = document.getElementById(el.getAttribute(`aria-controls`));
   menu.dispatchEvent(menuToggled)
 }
 
-function setupMenu(navigation)
-{
+function setupMenu(navigation) {
   if (navigation === null) {
     return
   }
 
-  let menuEl = navigation.menu
-  let toggleOpened = menuEl.dataset.opened || `Close`
-  let toggleClosed = menuEl.dataset.closed || `Open`
+  let menuEl = navigation.menu;
+  let toggleOpened = menuEl.dataset.opened || `Close`;
+  let toggleClosed = menuEl.dataset.closed || `Open`;
 
-  let items = menuEl.querySelectorAll(`li`)
-  let links = menuEl.querySelectorAll(`li > a`)
-  let submenus = menuEl.querySelectorAll(`li > ul`)
-  let toggles = menuEl.querySelectorAll(`[data-toggle]`)
-  let menus = menuEl.querySelectorAll(`nav ul`)
-  let topmenu = menuEl.querySelectorAll(`nav > ul`)
+  let items = menuEl.querySelectorAll(`li`);
+  let links = menuEl.querySelectorAll(`li > a`);
+  let submenus = menuEl.querySelectorAll(`li > ul`);
+  let toggles = menuEl.querySelectorAll(`[data-toggle]`);
+  let menus = menuEl.querySelectorAll(`nav ul`);
+  let topmenu = menuEl.querySelectorAll(`nav > ul`);
 
   // Initial parse for menus (all of them).
   menus.forEach(el => {
-    el.classList.add(`${elName}__menu`)
-    el.setAttribute(`hidden`, ``)
-    el.setAttribute(`id`, el.getAttribute(`id`) || shortid.generate())
+    el.classList.add(`${elName}__menu`);
+    el.setAttribute(`hidden`, ``);
+    el.setAttribute(`id`, el.getAttribute(`id`) || shortid.generate());
 
     // Listen for this menu being toggled.
     el.addEventListener(`menu-toggled`, handleMenuToggle)
-  })
+  });
 
   // Initial parse for top menu.
-  topmenu.forEach(el => {el.classList.add(`${elName}__menuTopmenu`)})
+  topmenu.forEach(el => {
+    el.classList.add(`${elName}__menuTopmenu`)
+  });
 
   // Initial parse for submenus.
   submenus.forEach(el => {
     el.classList.add(`${elName}__menuSubmenu`)
-  })
+  });
 
   // Initial parse for items.
   items.forEach(el => {
-    el.classList.add(`${elName}__menuItem`)
+    el.classList.add(`${elName}__menuItem`);
     if (el.querySelector(`ul`)) {
       el.classList.add(`${elName}__parent`)
     }
-  })
+  });
 
   // Initial parse for links.
   links.forEach(el => {
     el.classList.add(`${elName}__menuLink`)
-  })
+  });
 
   // Initial parse for toggles.
   toggles.forEach(el => {
-    el.classList.add(`${elName}__menuToggle`)
+    el.classList.add(`${elName}__menuToggle`);
 
     // Add an aria-controls attribute to the toggle button.
     let menu = el.nextElementSibling;
@@ -140,19 +140,25 @@ function setupMenu(navigation)
     }
 
     // Store opened/closed glyph info on the element.
-    el.dataset.opened = el.dataset.opened || toggleOpened
-    el.dataset.closed = el.dataset.closed || toggleClosed
+    el.dataset.opened = el.dataset.opened || toggleOpened;
+    el.dataset.closed = el.dataset.closed || toggleClosed;
 
     // Should always start closed.
-    el.textContent = el.dataset.closed
+    el.textContent = el.dataset.closed;
 
-    // Create CustomEvent to fire later.
-    let toggleClicked = new CustomEvent(`toggle-clicked`, { "bubbles": true, "cancelable": true,})
-    el.addEventListener(`toggle-clicked`, handleButtonToggle)
+    el.toggleMenuButton = function () {
+      this.dispatchEvent(new CustomEvent(`toggle-clicked`, {
+        "bubbles": true,
+        "cancelable": true,
+        "detail": {"menuid": this.getAttribute('aria-controls')}
+      }));
+    };
 
     // Set up a click listener.
-    el.addEventListener(`click`, button => el.dispatchEvent(toggleClicked))
-  })
+    el.addEventListener(`toggle-clicked`, handleButtonToggle);
+    el.addEventListener(`click`, button => button.target.toggleMenuButton())
+
+  });
 
   return {
     items: items,
@@ -167,43 +173,42 @@ const SiteNavigation = document.registerElement(
   {
     prototype: Object.create(
       HTMLElement.prototype, {
-      createdCallback: {value: function() {
-        console.log('here I am ^_^ ');
-        let navigation = getElements(this)
-        let menu = setupMenu(navigation)
-        console.log('with content: ', navigation);
-      }},
-      attachedCallback: {value: function() {
-        console.log('live on DOM ;-) ');
-
-        // Prevent events from leaking out into the wider DOM.
-        [`toggle-clicked`, `menu-toggled`, `toggle-state`, `menu-state`]
-          .map(event => this.addEventListener(event, e=> e.cancelBubble = true))
-      }},
-      detachedCallback: {value: function() {
-        console.log('leaving the DOM :-( )');
-      }},
-      attributeChangedCallback: {value: function(
-        name, previousValue, value
-      ) {
-        if (previousValue == null) {
-          console.log(
-            'got a new attribute ', name,
-            ' with value ', value
-          );
-        } else if (value == null) {
-          console.log(
-            'somebody removed ', name,
-            ' its value was ', previousValue
-          );
-        } else {
-          console.log(
-            name,
-            ' changed from ', previousValue,
-            ' to ', value
-          );
+        createdCallback: {
+          value: function () {
+            setupMenu(getElements(this));
+          }
+        },
+        attachedCallback: {
+          value: function () {
+            // Prevent events from leaking out into the wider DOM.
+            [`toggle-clicked`, `menu-toggled`, `toggle-state`, `menu-state`]
+              .map(event => this.addEventListener(event, e => e.cancelBubble = true))
+          }
+        },
+        detachedCallback: {
+          value: function () {}
+        },
+        attributeChangedCallback: {
+          value: function (name, previousValue, value) {
+            if (previousValue == null) {
+              console.log(
+                'got a new attribute ', name,
+                ' with value ', value
+              );
+            } else if (value == null) {
+              console.log(
+                'somebody removed ', name,
+                ' its value was ', previousValue
+              );
+            } else {
+              console.log(
+                name,
+                ' changed from ', previousValue,
+                ' to ', value
+              );
+            }
+          }
         }
-      }}
-    })
+      })
   }
 );
