f96b49f70e7a29c1963b8aaa96dd43d7161e6c90
Ben Martinez-Bateman <ben@alwaysblank.org>
2018-09-12 14:56:14 -0700

    Updated Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
+const path = require(`path`);
 
 module.exports = {
-  entry: './src/index.js',
+  entry: `./src/index.js`,
   output: {
-    filename: 'site-navigation.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: `site-navigation.js`,
+    path: path.resolve(__dirname, `dist`),
   },
   watch: true,
   module: {
@@ -13,20 +13,20 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: `babel-loader`,
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: [`@babel/preset-env`],
+          },
+        },
       },
       {
         test: /\.css$/,
         use: [
-          'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          'postcss-loader'
-        ]
-      }
-    ]
-  }
+          `style-loader`,
+          { loader: `css-loader`, options: { importLoaders: 1 } },
+          `postcss-loader`,
+        ],
+      },
+    ],
+  },
 };
